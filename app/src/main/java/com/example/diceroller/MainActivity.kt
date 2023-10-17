02d7d2b635d6f7234e5842e6f0ca73ff55702937
DanielGlez16 <danielgonzalezalcaide@gmail.com>
2023-10-17 10:45:57 +0200

    2 commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -13,9 +14,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,30 +34,56 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 DiceRollerApp()
 
-                }
             }
         }
     }
+}
 
 
 @Preview
 @Composable
 fun DiceRollerApp() {
-DiceWithButtonAndImage()
+    DiceWithButtonAndImage()
 }
 
+
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize()) {
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize()
+) {
+    var count by remember { mutableStateOf(0) }
+    var result by remember{ mutableStateOf(1) }
+    var resultanterior by remember { mutableStateOf(0) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Button(onClick = { /*TODO*/ }) {
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString()
+        )
+        Button(onClick = {resultanterior = result
+            result = (1..6).random()
+                         count=count+1
+                            }) {
             Text(stringResource(R.string.roll))
 
         }
+        Text(
+             text = "El dado se ha tirado " + count
+        )
+        Text(text = "El resultado fue " + resultanterior)
     }
 
 }
